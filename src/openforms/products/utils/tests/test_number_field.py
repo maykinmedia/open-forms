4abb31c2708499731cdd18c05cf5bbaf418d5337
tanja <tatjana@maykinmedia.nl>
2022-06-29 14:46:59 +0200

    :recycle: change formio fieldset classes; replace recurs to FieldSet attr; rewrite pytest to django tests

--- src/openforms/products/utils/tests/test_number_field.py
+++ src/openforms/products/utils/tests/test_number_field.py
@@ -1,22 +1,45 @@
-import pytest
-from tests.data.int_vs_number import integer_schema, number_schema
+from django.test import TestCase
 
-from src.entry import convert_json_schema_to_py
+from ..entry import convert_json_schema_to_py
 
 
-@pytest.mark.parametrize(
-    "schema,expected_output",
-    [
-        (integer_schema, ""),
-        (number_schema, None),
-    ],
-)
-def test_compair(schema, expected_output):
-    """JSON schema may contain type number and type integer;
-    if type == integer corresponding object of formIo will get key "integer"=""
-    in validate dict; otherwise this key will not be included.
-    """
-    assert (
-        convert_json_schema_to_py(schema)["components"][0]["validate"].get("integer")
-        == expected_output
-    )
+class NumberFieldTestCase(TestCase):
+    def setUp(self):
+        self.json_schema_integer = {
+            "required": ["huis_nummer"],
+            "properties": {
+                "huis_nummer": {
+                    "title": "home number",
+                    "type": "integer",
+                    "maximum": 1000,
+                    "minimum": 1,
+                }
+            },
+        }
+        self.json_schema_number = {
+            "required": ["prod_price"],
+            "properties": {
+                "prod_price": {
+                    "title": "Price",
+                    "type": "number",
+                    "maximum": 1000.00,
+                    "minimum": 0.01,
+                }
+            },
+        }
+
+    def test_integer(self):
+        result = convert_json_schema_to_py(self.json_schema_integer)
+        self.assertEqual(result["components"][0]["type"], "number")
+        self.assertEqual(result["components"][0]["validate"]["required"], True)
+        self.assertEqual(result["components"][0]["validate"]["max"], 1000)
+        self.assertEqual(result["components"][0]["validate"]["min"], 1)
+        self.assertEqual(result["components"][0].get("decimalLimit"), 0)
+
+    def test_number(self):
+        result = convert_json_schema_to_py(self.json_schema_number)
+        self.assertEqual(result["components"][0]["type"], "number")
+        self.assertEqual(result["components"][0]["validate"]["required"], True)
+        self.assertEqual(result["components"][0]["validate"]["max"], 1000.00)
+        self.assertEqual(result["components"][0]["validate"]["min"], 0.01)
+        self.assertEqual(result["components"][0].get("decimalLimit"), None)
